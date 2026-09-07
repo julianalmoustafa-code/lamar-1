08480b96349764b5e52607c5ba551262c5d269eb
julianalmoustafa-code <julianalmoustafa@gmail.com>
2026-09-07 19:27:42 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,7 @@ export const metadata: Metadata = {
     icon: '/lamar_icon.svg',
     shortcut: '/lamar_icon.svg',
   },
-  verification: {
-google: "Vln0-0yetaf2Z4Ujabbzp20guUmMBVP1sgQUXGjpZtY",
+  verification: {google: "vNn0-0yetaf2Z4UjabBzp2OguUuMbVP1sgQUtGjpZtY",
 },
   title: {
     default: 'LAMAR Stukadoor en Onderhoud | Gipswerk, Decoratie & Renovatie',
